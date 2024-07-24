5a7cee1a4ba886f77b083798d97d64e588fcd49f
Kirill Pet <job.mori.1967@gmail.com>
2024-07-24 12:23:58 +0300

    сверстал основу header и меню для телефона

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let btnMenuPhone = document.getElementById('btn-menu-phone');
+let blockMenu = document.getElementById('block-menu');
+btnMenuPhone.addEventListener('click', ()=>{
+    
+});

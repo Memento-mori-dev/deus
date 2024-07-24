204889be78903b34f99391ebb05171618a89a549
Kirill Pet <job.mori.1967@gmail.com>
2024-07-24 17:16:29 +0300

    сделал sub-header

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// меню для телефона
 let btnMenuPhone = document.getElementById('btn-menu-phone');
 let blockMenu = document.getElementById('block-menu');
 btnMenuPhone.addEventListener('click', (e)=>{
@@ -18,7 +19,39 @@ btnMenuPhone.addEventListener('click', (e)=>{
     blockMenu.classList.toggle('active');
 });
 
-// let menuHeader = document.getElementsByClassName('button-header');
-// menuHeader.addEventListener('click', (e)=>{
-//     console.log(e);
-// })
+// меню для пс
+let subHeader = document.getElementById('sub-header');
+
+document.getElementById('header-nav').addEventListener('click', (e)=>{
+    let button = e.target;
+    let links = e.currentTarget.querySelectorAll('a');
+
+    for (const link of links) {
+        if (link.classList.contains('active') && button != link) {
+            link.classList.remove('active');
+            break;
+        }
+    }
+
+    button.classList.toggle('active');
+
+    if (button.classList.contains('button-header_arrow')) {
+        let index = button.getAttribute('data-index');
+        let elements = subHeader.querySelectorAll('.sub-header__element');
+        let element = elements[index - 1];
+
+        for (const section of elements) {
+            if (section.classList.contains('active') && element != section) {
+                section.classList.remove('active');
+            }
+        }
+
+        element.classList.toggle('active');
+
+        if (subHeader.querySelectorAll('.sub-header__element.active').length > 0) {
+            subHeader.classList.add('active');
+        } else {
+            subHeader.classList.remove('active');
+        }
+    }
+})

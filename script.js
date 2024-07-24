0e1d3f81bdf109d08fa0603f3ff4c5ffa7d193a1
Kirill Pet <job.mori.1967@gmail.com>
2024-07-24 13:13:09 +0300

    закончил верстку мобильного header

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 
 let btnMenuPhone = document.getElementById('btn-menu-phone');
 let blockMenu = document.getElementById('block-menu');
-btnMenuPhone.addEventListener('click', ()=>{
-    
+btnMenuPhone.addEventListener('click', (e)=>{
+    let button = e.currentTarget;
+    let links = blockMenu.querySelectorAll('a');
+
+    for (const link of links) {
+        if (link.getAttribute('tabindex') == -1) {
+            link.setAttribute('tabindex', '1');
+        }else{
+            link.setAttribute('tabindex', '-1');
+        }
+    }
+
+    button.classList.toggle('active');
+    blockMenu.classList.toggle('active');
 });
